17f88bf638199a5530be0e11b138af9a956e4194
Nan Zhong <nan@nine27.com>
2014-11-20 15:40:52 -0500

    Add heroku page

---
+++ app/controllers/heroku_controller.rb
@@ -0,0 +1,5 @@
+class HerokuController < ApplicationController
+  def index
+    render layout: false
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ SampleApp::Application.routes.draw do
   resources :sessions,      only: [:new, :create, :destroy]
   resources :microposts,    only: [:create, :destroy]
   resources :relationships, only: [:create, :destroy]
-  root to: 'static_pages#home'
+  root to: 'heroku#index'
   match '/signup',  to: 'users#new',            via: 'get'
   match '/signin',  to: 'sessions#new',         via: 'get'
   match '/signout', to: 'sessions#destroy',     via: 'delete'
